395c80db60ae72bb4aac456e4dec1e290ef18461
NiraliPriti <niralipritiben@gmail.com>
2026-09-03 03:51:15 +0000

    feat:python folder has been created

---
+++ Python/hello.py
@@ -0,0 +1,50 @@
+
+# Python learning 
+print("Hello Ganesha :)")
+# we have downloaded auto pep8 and installed it in our system. Now we can use it to format our code.
+# we can have ultiple line comments here using #
+# comments are important for understanding the code.
+x = 10 # final exam score this is a inline comment
+# python ignore all values after # in a line.
+
+#functions in python
+# BUILT IN FUNCTIONS ex. print(), input(), len(), type(), range() etc
+# Third party Libraries functions ex. numpy(), pandas(), matplotlib() etc
+#user defined functions ex. def my_function():, def add(a,b): etc
+print('Hello message in single quotes')
+print("----------------------")
+print("   LEARN PYTHON   ")
+print("----------------------")
+
+
+#  Special Characters 
+print( " HI \"python\" ")
+print("Hi 'Python ' ")
+print("Path: C:\\Users\\Nirali") #to print backslash we use \\ double backslash :)
+
+print("Message 1 \nMessage 2")
+print("Message 3\tMessage 4")
+
+# Python Print() Challenge
+print("""Your Learning Path: 
+      \t- PythonBasics
+      \t- Data Engineering
+      \t- AI""")
+
+      
+      
+#VARIABLES IN PYTHON 
+name = "Nirali"
+print("My name is ", name)
+print(name , "is learning Python")
+print(name , " wants to become python expert")
+#variable makes updates super easy one change updte everything.
+name = "Priti"
+language = "javascript"
+print("My name is ", name)
+print(name , "is learning", language)
+print(name , " wants to become", language, "expert")
+
+# 💡 Don't repeat it - Name it!
+# if you find yourself using the same value over and over ,store it in a variable to make your code easier to change  !
+
